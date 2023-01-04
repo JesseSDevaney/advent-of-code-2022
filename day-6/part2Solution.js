3ecd8fd699f2ecc70df712fae77e8ea8e574f8a9
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-01-04 16:04:26 -0500

    day 6 - part 2 submission

--- day-6/part2Solution.js
+++ day-6/part2Solution.js
@@ -2,31 +2,29 @@ import * as fs from "node:fs";
 
 const input = fs.readFileSync("./puzzleInput.txt", "utf-8");
 
-const [startOfPacketMarker, index] = getStartOfPacket(input);
+const [startOfMessageMarker, index] = NDistinctCharsInARow(input, 14);
 
-console.log(`Start of packet marker: ${startOfPacketMarker}`);
-console.log(`First marker after character: ${index}`);
+console.log(`Start of message marker: ${startOfMessageMarker}`);
+console.log(`First marker after character count: ${index}`);
 
 // * HELPER FUNCTIONS
-function getStartOfPacket(input) {
-  // start of packet marker is four characters in a row that
-  // are all different
-  let startOfPacketMarker = "";
-  let readCharCount = 0;
+function NDistinctCharsInARow(input, numOfDistinctChars) {
+  let charAccumulator = "";
+  let charCount = 0;
 
   for (let char of input) {
-    if (startOfPacketMarker.includes(char)) {
-      const duplicateIndex = startOfPacketMarker.indexOf(char);
+    if (charAccumulator.includes(char)) {
+      const duplicateIndex = charAccumulator.indexOf(char);
 
       // remove all characters at and before duplicate
       // b/c it can no longer contribute to a start-of-packet marker
-      startOfPacketMarker = startOfPacketMarker.slice(duplicateIndex + 1);
+      charAccumulator = charAccumulator.slice(duplicateIndex + 1);
     }
 
-    startOfPacketMarker += char;
-    readCharCount++;
-    if (startOfPacketMarker.length === 4) break;
+    charAccumulator += char;
+    charCount++;
+    if (charAccumulator.length === numOfDistinctChars) break;
   }
 
-  return [startOfPacketMarker, readCharCount];
+  return [charAccumulator, charCount];
 }
