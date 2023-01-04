52a17e1f7d714946c71e38c96c974654e8f5918a
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-01-04 15:54:45 -0500

    day 6 - part 1 submission

---
+++ day-6/part1Solution.js
@@ -0,0 +1,25 @@
+import * as fs from "node:fs";
+
+const input = fs.readFileSync("./puzzleInput.txt", "utf-8");
+
+// start of packet marker is four characters in a row that
+// are all different
+let startOfPacketMarker = "";
+let readCharCount = 0;
+
+for (let char of input) {
+  if (startOfPacketMarker.includes(char)) {
+    const duplicateIndex = startOfPacketMarker.indexOf(char);
+
+    // remove all characters at and before duplicate
+    // b/c it can no longer contribute to a start-of-packet marker
+    startOfPacketMarker = startOfPacketMarker.slice(duplicateIndex + 1);
+  }
+
+  startOfPacketMarker += char;
+  readCharCount++;
+  if (startOfPacketMarker.length === 4) break;
+}
+
+console.log(`Start of packet marker: ${startOfPacketMarker}`);
+console.log(`First marker after character: ${readCharCount}`);
