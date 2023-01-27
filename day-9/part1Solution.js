5c53669ac7af1b71731613374df8c0d80bcac0d2
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-01-27 13:59:27 -0500

    day 9 - part 1 (setup simulation boilerplate)

--- day-9/part1Solution.js
+++ day-9/part1Solution.js
@@ -11,3 +11,32 @@ const movesTaken = input.split(/\n/).map((move) => {
   };
 });
 
+const simulationHistory = simulate(movesTaken);
+
+console.log(simulationHistory);
+
+// ***** Functional code *****
+
+function simulate(moves) {
+  const INITIAL_X = 0;
+  const INITIAL_Y = 0;
+
+  let currHead = new Map([
+    ["x", INITIAL_X],
+    ["y", INITIAL_Y],
+  ]);
+  let currTail = new Map([
+    ["x", INITIAL_X],
+    ["y", INITIAL_Y],
+  ]);
+
+  const simulationHistory = [
+    {
+      head: currHead,
+      tail: currTail,
+    },
+  ];
+
+  return simulationHistory;
+}
+
