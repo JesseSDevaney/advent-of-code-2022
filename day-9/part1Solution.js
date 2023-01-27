bead4f6847b08ce0aa6e3f84cc05c94e0cc9ea16
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-01-27 13:59:48 -0500

    day 9 - part 1 (add knot movements)

--- day-9/part1Solution.js
+++ day-9/part1Solution.js
@@ -37,6 +37,76 @@ function simulate(moves) {
     },
   ];
 
+  for (const { direction, steps } of moves) {
+    let stepsRemaining = steps;
+    while (stepsRemaining > 0) {
+      const { nextHead, nextTail } = moveKnotsOneStep(
+        currHead,
+        currTail,
+        direction
+      );
+
+      currHead = nextHead;
+      currTail = nextTail;
+
+      simulationHistory.push({
+        currHead,
+        currTail,
+      });
+
+      stepsRemaining--;
+    }
+  }
+
   return simulationHistory;
 }
 
+function moveKnotsOneStep(currHead, currTail, direction) {
+  const nextHead = moveHead(currHead, direction);
+  const nextTail = moveTail(currTail, nextHead);
+
+  return { nextHead, nextTail };
+}
+
+function moveHead(head, direction) {
+  const UP = "U";
+  const DOWN = "D";
+  const LEFT = "L";
+  const RIGHT = "R";
+
+  const nextHead = new Map(head);
+
+  if (direction === UP) nextHead.set("y", nextHead.get("y") + 1);
+  if (direction === DOWN) nextHead.set("y", nextHead.get("y") - 1);
+  if (direction === LEFT) nextHead.set("x", nextHead.get("x") - 1);
+  if (direction === RIGHT) nextHead.set("x", nextHead.get("x") + 1);
+
+  return nextHead;
+}
+
+function moveTail(currTail, nextHead) {
+  const nextTail = new Map(currTail);
+
+  const nextHeadX = nextHead.get("x");
+  const nextHeadY = nextHead.get("y");
+  const currTailX = currTail.get("x");
+  const currTailY = currTail.get("y");
+
+  const diffX = nextHeadX - currTailX;
+  const diffY = nextHeadY - currTailY;
+
+  const isXMove =
+    Math.abs(diffX) > 1 || (Math.abs(diffY) > 1 && Math.abs(diffX) === 1);
+  const isYMove =
+    Math.abs(diffY) > 1 || (Math.abs(diffX) > 1 && Math.abs(diffY) === 1);
+
+  if (isXMove) {
+    nextTail.set("x", currTailX + diffX / Math.abs(diffX));
+  }
+
+  if (isYMove) {
+    nextTail.set("y", currTailY + diffY / Math.abs(diffY));
+  }
+
+  return nextTail;
+}
