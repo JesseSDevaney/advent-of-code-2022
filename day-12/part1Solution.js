9f585d03b8c9e967b7731f2f35c6eed1733f36a6
Jesse Devaney <22608765+JesseSDevaney@users.noreply.github.com>
2023-02-06 16:00:30 -0500

    implement findStartingPosition

--- day-12/part1Solution.js
+++ day-12/part1Solution.js
@@ -1,6 +1,21 @@
 import * as fs from "node:fs";
 
+const CURRENT_POSITION = "S";
+const GOAL = "E";
+
 const input = fs.readFileSync("./puzzleInput.txt", "utf-8");
 const grid = input.split(/\n/).map((row) => row.split(""));
 
-console.log(grid);
+const startingPosition = findStartingPosition(grid);
+
+console.log(startingPosition);
+
+function findShortestPath(grid, startingPosition) {}
+
+function findStartingPosition(arr2D) {
+  for (let row = 0; row < arr2D.length; row++) {
+    for (let col = 0; col < arr2D[row].length; col++) {
+      if (arr2D[row][col] === CURRENT_POSITION) return { row, col };
+    }
+  }
+}
